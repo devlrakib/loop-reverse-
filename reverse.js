81688291b4d15867cc29f5aa5bb314d75df4954b
devlrakib <devrakib5@gmail.com>
2024-03-15 15:15:15 +0600

    first commit

---
+++ reverse.js
@@ -0,0 +1,20 @@
+// var number = 1;
+// while (number <= 10){
+//     console.log(number);
+//     number++;
+// }
+
+// !While Loop Reverse Way
+
+// var num = 10;
+// while (num >= 1){
+//     console.log(num);
+//     num--;
+// }
+// for ( var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+// ! For Loop Reverse Way
+for (var i = 10; i >=1; i--){
+    console.log(i);
+}
